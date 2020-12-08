a135a436db90d3f9f2ef57831600c375a84561e7
rbhogal <rbhogal23gmail.com>
2020-12-08 08:38:21 -0800

    named and default import and exports

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 // Importing module
-import { addToCart } from './shoppingCart.js';
-console.log('Importing modules');
+// import { addToCart, totalPrice as price, tq} from './shoppingCart.js';
+// addToCart('bread', 5);
+// console.log(price, tq);
 
-addToCart('bread', 5);
+console.log('Importing modules');
+// console.loc(shippingCost);
+
+// import * as ShoppingCart from './shoppingCart.js';
+// ShoppingCart.addToCart('bread', 5);
+// console.log(ShoppingCart.totalPrice);
+
+// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// console.log(price);
+
+import add, {cart} from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apple', 4);
+
+console.log(cart);
--- shoppingCart.js
+++ shoppingCart.js
@@ -2,9 +2,21 @@
 console.log('Exporting module');
 
 const shippingCost = 10;
-const cart = [];
+export const cart = [];
 
 export const addToCart = function (product, quantity) {
   cart.push({ product, quantity });
   console.log(`${quantity} ${product} added to cart`);
 };
+
+const totalPrice = 237;
+const totalQuantity = 23;
+
+export { totalPrice, totalQuantity as tq};
+
+export default function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(`${quantity} ${product} added to cart`);
+}
+
+
